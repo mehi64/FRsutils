c72a37e58972f3b636f6824bc365b7405959f335
Mehran_Amiri_HLS_Account <amiri@hybrid-lidar.com>
2025-05-20 14:01:23 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="FRsutils",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.21.0",
